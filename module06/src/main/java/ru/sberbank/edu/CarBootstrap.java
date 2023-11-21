f74ec04cdc546e6cfacba042beb615dc0eff7a34
other95 <other95@gmail.com>
2023-11-21 23:03:44 +0300

    Решение задач модуля 06

--- module06/src/main/java/ru/sberbank/edu/CarBootstrap.java
+++ module06/src/main/java/ru/sberbank/edu/CarBootstrap.java
@@ -3,6 +3,7 @@ package ru.sberbank.edu;
 
 import org.h2.tools.Server;
 import ru.sberbank.edu.dbconnection.H2DbEmbedded;
+import ru.sberbank.edu.model.Car;
 import ru.sberbank.edu.repository.CarDbRepositoryImpl;
 import ru.sberbank.edu.repository.CarRepository;
 import ru.sberbank.edu.service.CarService;
@@ -10,6 +11,8 @@ import ru.sberbank.edu.service.CarServiceImpl;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CarBootstrap {
     public static void main(String[] args) throws Exception {
@@ -33,7 +36,39 @@ public class CarBootstrap {
                 System.out.println("id=" + id + "; model=" + model);
             }
             // Test end
+
+            //Начало теста 2
+
+            carRepository.deleteAll();
+            Set<Car> carsToAdd = new HashSet<>();
+            carsToAdd.add(new Car("111", "Хундай"));
+            carsToAdd.add(new Car("222", "Ренаулт"));
+            carsToAdd.add(new Car("333", "Пугеот"));
+            carsToAdd.add(new Car("112", "Хундай"));
+            carsToAdd.add(new Car("156", "Cherry"));
+            carRepository.createAll(carsToAdd);
+            Set<Car> cars = carRepository.findAll();
+            System.out.println("Список автомобилей");
+            for (Car car:cars) {
+                System.out.println(car);
+            }
+            System.out.println();
+
+            System.out.println("Список автомобилей модели Хундай");
+            cars = carRepository.findByModel("Хундай");
+            for (Car car:cars) {
+                System.out.println(car);
+            }
+
+            carService.deleteCar("156");
+            System.out.println("Список автомобилей после удаления ид 156");
+            cars = carRepository.findAll();
+            for (Car car:cars) {
+                System.out.println(car);
+            }
+            //Конец теста 2
         }
         server.stop();
+
     }
 }
